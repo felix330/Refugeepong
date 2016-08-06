0cecd9e456465a8f9c40213c89831df7c4457923
Felix Gähr <felix330@gmail.com>
2016-08-06 16:14:34 +0200

    Paddle anim

--- Assets/rippleAnim.cs
+++ Assets/rippleAnim.cs
@@ -3,14 +3,38 @@ using System.Collections;
 
 public class rippleAnim : MonoBehaviour {
 
-
+	public Sprite[] rippleStates;
+	private int currentState;
+	public int interval;
+	private float counter;
 	// Use this for initialization
 	void Start () {
-	
+		currentState = 0;
+		counter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		counter += 1*Time.deltaTime;
+
+		if (counter >= interval)
+		{
+			switchState();
+			counter = 0;
+		}
+	}
+
+	private void switchState()
+	{
+		if (currentState < rippleStates.Length-1)
+		{
+			currentState += 1;
+			GetComponent<SpriteRenderer>().sprite = rippleStates[currentState];
+		}
+		else
+		{
+			currentState = 0;
+			GetComponent<SpriteRenderer>().sprite = rippleStates[0];
+		}
 	}
 }
