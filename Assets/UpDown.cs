da462d8c44eac27759e3a58c98163edd6415e8f3
Felix Gähr <felix330@gmail.com>
2016-08-06 11:50:06 +0200

    Added player and paddle prefabs

---
+++ Assets/UpDown.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpDown : MonoBehaviour {
+
+	public float sensitivity;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void up()
+	{
+		transform.position += transform.up*sensitivity;
+	}
+
+	void down()
+	{
+		transform.position -= transform.up*sensitivity;
+	}
+}
